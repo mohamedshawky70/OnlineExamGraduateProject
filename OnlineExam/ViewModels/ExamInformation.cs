89ad0d715d4014962482b0fc8ebf02cab1d2eb87
ahmed <ahmed@gmail.com>
2023-06-24 18:01:24 +0300

    add answers page

---
+++ OnlineExam/ViewModels/ExamInformation.cs
@@ -0,0 +1,15 @@
+﻿using OnlineExam.Models;
+
+namespace OnlineExam.ViewModels
+{
+    public class ExamInformation
+    {
+        public int NumOfQuestions { set; get; }
+        public string ExamName { set; get; }
+        public DateTime ExamDate { set; get; }
+
+        public AnswerQuestion question { set; get; }
+
+
+    }
+}
