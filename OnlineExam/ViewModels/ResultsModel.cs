f69dac0b67c33fa8e798ecb6b85967c6485b52b6
kareem <kareemabdelrhmane@gmail.com>
2023-06-24 12:01:57 +0300

    add answers page

--- OnlineExam/ViewModels/ResultsModel.cs
+++ OnlineExam/ViewModels/ResultsModel.cs
@@ -6,13 +6,12 @@ namespace OnlineExam.ViewModels
     {
         public string ExamName { set; get; }
         public List<Answer> answers { set; get; }
-
-        public int passed { set; get; }
+        public int numberOfPassedStudent { set; get; }
+        public int numberOfFailedStudent { set; get; }   
+        public List<bool> isStudentPassed { set; get; }
         public int averageGrade { set; get; }
 
-        public int Failed { set; get; }
-
-
+        public List<String> StudentsStatus { set; get; }
 
     }
 }
